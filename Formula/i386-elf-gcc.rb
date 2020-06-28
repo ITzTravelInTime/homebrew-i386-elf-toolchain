51f00a738b171899cc9dc3c9c28435e6e503ee27
ITzTravelInTime <piecaruso97@gmail.com>
2020-06-28 19:34:53 +0200

    Added checksum

--- Formula/i386-elf-gcc.rb
+++ Formula/i386-elf-gcc.rb
@@ -3,7 +3,7 @@ class I386ElfGcc < Formula
   homepage "https://gcc.gnu.org"
   url "http://ftpmirror.gnu.org/gcc/gcc-9.3.0/gcc-9.3.0.tar.xz"
   version "9.3.0"
-  #sha256 "ea6ef08f121239da5695f76c9b33637a118dcf63e24164422231917fa61fb206"
+  sha256 "71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1"
 
 
   depends_on "gmp" => :build
